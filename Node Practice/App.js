598fb13b660eae75c270f1a0ce89b016969a7338
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-07 06:20:35 +0000

    feat: file system module in node

--- Node Practice/App.js
+++ Node Practice/App.js
@@ -1,5 +1,6 @@
 const log = require("./script");
 const os = require('os');
+const fs = require('fs');
 
 // log("Hello World!");
 
@@ -8,4 +9,9 @@ const path = require("path");
 let pathObj = path.parse(__filename);
 // console.log(pathObj);
 
-console.log(os.uptime());
+// console.log(os.uptime());
+
+console.log(fs.readdir('./',(err,files) => {
+    if(err) console.log("Error: ",err);
+    else console.log(files);
+}))
