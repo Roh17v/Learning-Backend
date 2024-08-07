b5b5596537faa078ffc54403c13abf87e52eed88
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-07 06:53:20 +0000

    feat: event emmitter and listner

--- Node Practice/App.js
+++ Node Practice/App.js
@@ -1,6 +1,17 @@
 const log = require("./script");
-const os = require('os');
-const fs = require('fs');
+const os = require("os");
+const fs = require("fs");
+const EventEmmitter = require("events");
+const event = new EventEmmitter();
+
+// listening for an event named message logged
+event.on("message logged", () => {
+  console.log("Event Listened");
+});
+
+// emitting an event named message logged
+event.emit("message logged");
+
 
 // log("Hello World!");
 
@@ -11,7 +22,7 @@ let pathObj = path.parse(__filename);
 
 // console.log(os.uptime());
 
-console.log(fs.readdir('./',(err,files) => {
-    if(err) console.log("Error: ",err);
-    else console.log(files);
-}))
+// console.log(fs.readdir('./',(err,files) => {
+//     if(err) console.log("Error: ",err);
+//     else console.log(files);
+// }))
