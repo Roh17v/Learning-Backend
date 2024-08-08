aa43ff74c44bab2605c02de1ec6d2b2985060fc8
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-08 06:18:03 +0000

    feat: server using http module

--- Node Practice/App.js
+++ Node Practice/App.js
@@ -1,35 +1,21 @@
-// const log = require("./script");
-const os = require("os");
-const fs = require("fs");
-const EventEmmitter = require("events");
-const event = new EventEmmitter();
+const http = require("http");
 
-// listening for an event named message logged
-event.on("message logged", (args) => {
-  console.log("Event Listened", args);
+const server = http.createServer((req, res) => {
+  if (req.url === "/") {
+    res.write("Hello World!");
+    res.end();
+  }
+
+  if (res.url === "/api") {
+    res.write(JSON.stringify([1, 2, 3, 4]));
+    res.end();
+  }
 });
 
-// emitting an event named message logged
-event.emit("message logged", { id: 1, url: "http://example.com" });
-
-// log("Hello World!");
-
-const path = require("path");
-
-let pathObj = path.parse(__filename);
-// console.log(pathObj);
-
-// console.log(os.uptime());
-
-// console.log(fs.readdir('./',(err,files) => {
-//     if(err) console.log("Error: ",err);
-//     else console.log(files);
-// }))
-
-const Logger = require("./TempFolder/logger");
-
-const log = new Logger();
-
-log.on("message logged", () => {
-  console.log("Message Event Listened");
+server.on("connection", (socket) => {
+  console.log("New Connection!");
 });
+
+server.listen(3000);
+
+console.log("Listening on port: 3000");
