23964da8c3a63e780a54f49e3ee381a44508227d
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-09 14:32:17 +0000

    feat: handled delete request

--- Express/index.js
+++ Express/index.js
@@ -27,24 +27,52 @@ app.get("/api/courses/:id", (req, res) => {
 });
 
 app.post("/api/courses", (req, res) => {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
+  const { error } = validateSchema(req.body);
 
-  const result = schema.validate(req.body);
-  console.log(result);
-
-  if (result.error) {
-    res.status(400).send(result.error.details[0].message);
-    return;
+  if (error) {
+    return res.status(400).send(error.details[0].message);
   }
+
   const course = {
     id: courses.length + 1,
     name: req.body.name,
   };
   courses.push(course);
-  res.send(result.value);
+  res.send(course);
 });
+
+app.put("/api/courses/:id", (req, res) => {
+  const course = courses.find((c) => c.id === parseInt(req.params.id));
+  if (!course) return res.status(404).send("Not Found");
+
+  const { error } = validateSchema(req.body);
+  if (error) {
+    return res.status(400).send(error.details[0].message);
+  }
+
+  course.name = req.body.name;
+  res.send(course);
+});
+
+app.delete('/api/courses/:id',(req, res) => {
+  const course = courses.find((course) => course.id === parseInt(req.params.id))
+  if(!course)
+  {
+    return res.status(404).send("Not Found");
+  }
+
+  const index = courses.indexOf(course);
+  courses.splice(index,1);
+  res.send(course);
+})
+
+function validateSchema(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+  });
+
+  return schema.validate(course);
+}
 
 const port = process.env.PORT || 3000;
 
