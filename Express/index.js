605a4cc4ccd9d5b90a063848671c4fe0d5998077
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-10 07:13:25 +0000

    refactor: moved '/api/courses' routes to routes/courses.js file

--- Express/index.js
+++ Express/index.js
@@ -1,12 +1,14 @@
 const express = require("express");
-const Joi = require("joi");
 const config = require('config')
 const app = express();
+const courses = require('./routes/courses')
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+app.use('/api/courses', courses);
+
 console.log(process.env.NODE_ENV)
 console.log(app.get("env"));
 console.log(config.get('dbConfig.host'))
@@ -17,74 +19,11 @@ app.use(function (req, res, next) {
   next();
 });
 
-const courses = [
-  { id: 1, name: "course1" },
-  { id: 2, name: "course2" },
-  { id: 3, name: "course3" },
-];
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.get("/api/courses", (req, res) => {
-  res.send(courses);
-});
-
-app.get("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) res.status(404).send("Not Found");
-  res.send(course);
-});
-
-app.post("/api/courses", (req, res) => {
-  const { error } = validateSchema(req.body);
-
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  const course = {
-    id: courses.length + 1,
-    name: req.body.name,
-  };
-  courses.push(course);
-  res.send(course);
-});
-
-app.put("/api/courses/:id", (req, res) => {
-  const course = courses.find((c) => c.id === parseInt(req.params.id));
-  if (!course) return res.status(404).send("Not Found");
-
-  const { error } = validateSchema(req.body);
-  if (error) {
-    return res.status(400).send(error.details[0].message);
-  }
-
-  course.name = req.body.name;
-  res.send(course);
-});
-
-app.delete("/api/courses/:id", (req, res) => {
-  const course = courses.find(
-    (course) => course.id === parseInt(req.params.id)
-  );
-  if (!course) {
-    return res.status(404).send("Not Found");
-  }
-
-  const index = courses.indexOf(course);
-  courses.splice(index, 1);
-  res.send(course);
-});
-
-function validateSchema(course) {
-  const schema = Joi.object({
-    name: Joi.string().min(3).required(),
-  });
-
-  return schema.validate(course);
-}
 
 const port = process.env.PORT || 3000;
 
