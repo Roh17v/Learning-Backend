739a9c3996a527df4db9f9697ce414e8af98ab8f
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-12 16:51:44 +0000

    feat: promise all and promise race

--- Async Javascript/callback-api.js
+++ Async Javascript/callback-api.js
@@ -2,14 +2,15 @@ const p1 = new Promise((resovle, reject) => {
     setTimeout(() => {
         console.log("Async Function 1...");
         resovle(1);
-    },2000)
+    },5000)
 })
 
 
 const p2 = new Promise((resovle, reject) => {
     setTimeout(() => {
         console.log("Async Function 2...");
-        resovle(1);
+        resovle(2);
     },2000)
 })
 
+Promise.race([p1,p2]).then((result) => console.log(result));
