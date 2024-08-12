554b2714477714d0688da134e5da445717b3e406
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-12 05:21:43 +0000

    feat: Add function to demonstrate Callback Hell problem

--- Async Javascript/index.js
+++ Async Javascript/index.js
@@ -1,6 +1,9 @@
 console.log("Before");
 getUser(1, (user) => {
   console.log(user);
+  getRepos(user.name, (repos) => {
+    console.log('Repos: ',repos)
+  })
 });
 console.log("After");
 
@@ -9,4 +12,10 @@ function getUser(id, callback) {
     console.log("Fetching users from the database");
     callback({ id: id, gitHubUserName: "Rohit" });
   }, 3000);
+}
+
+function getRepos(username, callback) {
+  setTimeout(() => {
+    callback(["repo1", "repo2", "repo3"]);
+  }, 2000);
 }
