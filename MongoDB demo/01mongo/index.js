2a2c0f5003eba6d31c42a6d2c6b03c61f0db0538
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-14 09:58:45 +0000

    feat: saving data into database using course creation endpoint

--- MongoDB demo/01mongo/index.js
+++ MongoDB demo/01mongo/index.js
@@ -2,9 +2,14 @@ import dotenv from "dotenv";
 import express from "express";
 import connectDB from "./src/db/index.js";
 import { Course } from "./schemas/course.schema.js";
+import Joi from "joi";
 
 const app = express();
 dotenv.config();
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 connectDB()
   .then(() => {
@@ -25,23 +30,50 @@ app.get("/hello", (req, res) => {
   res.send("Hello");
 });
 
-// async function createUser() {
-//   const course = new Course({
-//     name: "Angular",
-//     author: "Mosh Hemadani",
-//     tags: ["Angular", "frontend"],
-//     isPublished: true,
-//   });
+app.post("/createcourse", (req, res) => {
+  const { error } = validateCourse(req.body);
+  console.log(error);
+  if (error) return res.status(400).send(error.details[0].message);
 
-//   const result = await course.save();
+  const course = new Course(req.body);
+  const result = course
+    .save()
+    .then((result) => {
+      console.log(result);
+      return res.status(201).send(result);
+    })
+    .catch((error) => {
+      console.error("Error creating course:", error);
+      return res
+        .status(500)
+        .send("Failed to create course. Please try again later.");
+    });
+});
+
+function validateCourse(course) {
+  const schema = Joi.object({
+    name: Joi.string().min(3).required(),
+    author: Joi.string().min(3).required(),
+    tags: Joi.array(),
+    isPublished: Joi.boolean().required(),
+  });
+
+  return schema.validate(course);
+}
+
+// async function createUser(course) {
+//   const courseData = new Course(course);
+
+//   const result = await courseData.save();
 //   console.log(result);
+//   return result;
 // }
 
 // createUser();
 
-async function getUser() {
-  const courses = await Course.find({ author: /^mosh/i }).countDocuments();
-  console.log(courses);
-}
+// async function getUser() {
+//   const courses = await Course.find({ author: /^Kunal/i });
+//   console.log(courses);
+// }
 
-getUser();
+// getUser();
