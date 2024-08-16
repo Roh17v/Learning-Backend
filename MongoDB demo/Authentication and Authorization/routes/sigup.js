c93b751a04af837885d65bb160968e8d9b2fafd1
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-16 17:29:42 +0000

    feat: Hashing password using bcrypt

--- MongoDB demo/Authentication and Authorization/routes/sigup.js
+++ MongoDB demo/Authentication and Authorization/routes/sigup.js
@@ -1,11 +1,11 @@
 import { User, validateUser } from "../models/user.model.js";
 import _ from "lodash";
 import express from "express";
+import bcrypt from "bcrypt";
 
 const signupRouter = express.Router();
 
 signupRouter.post("/", async (req, res) => {
-  console.log(req.body);
   const { error } = validateUser(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -15,8 +15,9 @@ signupRouter.post("/", async (req, res) => {
   try {
     const newUser = new User(_.pick(req.body, ["name", "email", "password"]));
 
+    const salt = await bcrypt.genSalt(10);
+    newUser.password = await bcrypt.hash(newUser.password, salt);
     await newUser.validate();
-
     await newUser.save();
     return res.status(201).send(_.pick(newUser, ["_id", "name", "email"]));
   } catch (error) {
