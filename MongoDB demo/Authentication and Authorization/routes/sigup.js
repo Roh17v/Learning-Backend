2466ec499532d2c1be5aa2b5770a0113b44e7b14
Rohit Verma <137288929+Roh17v@users.noreply.github.com>
2024-08-16 17:11:09 +0000

    feat: Using lodash for utility function

--- MongoDB demo/Authentication and Authorization/routes/sigup.js
+++ MongoDB demo/Authentication and Authorization/routes/sigup.js
@@ -1,5 +1,5 @@
 import { User, validateUser } from "../models/user.model.js";
-import mongoose from "mongoose";
+import _ from "lodash";
 import express from "express";
 
 const signupRouter = express.Router();
@@ -13,16 +13,12 @@ signupRouter.post("/", async (req, res) => {
   if (user) return res.status(400).send("User already registered.");
 
   try {
-    const newUser = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    const newUser = new User(_.pick(req.body, ["name", "email", "password"]));
 
     await newUser.validate();
 
     await newUser.save();
-    return res.status(201).send(newUser);
+    return res.status(201).send(_.pick(newUser, ["_id", "name", "email"]));
   } catch (error) {
     console.log("Failed to Register the User: ", error);
     return res.status(500).send("Failed to Register the User");
